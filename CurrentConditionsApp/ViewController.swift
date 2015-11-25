7e14c5af5545b94cb8ed1df2310ef81040189f44
Mary Grace Lucas <graceylucas@hotmail.com>
2015-11-24 23:41:42 -0500

    App works, includes error messages

--- CurrentConditionsApp/ViewController.swift
+++ CurrentConditionsApp/ViewController.swift
@@ -9,17 +9,86 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var enterCityTextField: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func findOutButtonPressed(sender: UIButton) {
+        
+        var wasSuccessful = false
+        
+        let attemptedUrl = NSURL(string: "http://www.weather-forecast.com/locations/" + enterCityTextField.text!.stringByReplacingOccurrencesOfString(" ", withString: "-") + "/forecasts/latest")
+        
+        if let url = attemptedUrl {
+            
+            let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) -> Void in
+                
+                if let urlContent = data {
+                    
+                    let webContent = NSString(data: urlContent, encoding: NSUTF8StringEncoding)
+                    
+                    let websiteArray = webContent!.componentsSeparatedByString("3 Day Weather Forecast Summary:</b><span class=\"read-more-small\"><span class=\"read-more-content\"> <span class=\"phrase\">")
+                    
+                    if websiteArray.count > 1 {
+                        let weatherArray = websiteArray[1].componentsSeparatedByString("</span>")
+                        
+                        if weatherArray.count > 1 {
+                            
+                            wasSuccessful = true
+                            
+                            let weatherSummary = weatherArray[0].stringByReplacingOccurrencesOfString("&deg;", withString: "º")
+                            
+                            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                                
+                                self.resultLabel.text = weatherSummary
+                                
+                                print(weatherSummary)
+                                
+                                self.resultLabel.hidden = false
+                                
+                                if wasSuccessful == false {
+                                    
+                                    self.resultLabel.text = "Could not find weather for this ciy. Please try again."
+                                    
+                                    self.resultLabel.hidden = false
+                                    
+                                }
+                                
+                            })
+                            
+                        }
+                        
+                    }
+                    
+                }
+                
+            }
+            
+            task.resume()
+        }
+        else {
+            
+            self.resultLabel.text = "Could not find weather for this ciy. Please try again."
+            
+            self.resultLabel.hidden = false
+            
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
